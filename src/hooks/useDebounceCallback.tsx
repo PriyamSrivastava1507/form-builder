d8b64e7f81bcd77825e5ebcb17043018072a8cbd
PriyamSrivastava1507 <priyam.srivastava1575@gmail.com>
2026-03-24 23:34:30 +0530

    feat: create Debounce Callback hook

---
+++ src/hooks/useDebounceCallback.tsx
@@ -0,0 +1,20 @@
+import { useRef, useCallback } from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function useDebounceCallback<T extends (...args: any[]) => void>(
+    callback: T,
+    delay: number
+) {
+    const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+    const debouncedCallback = useCallback((...args: Parameters<T>) => {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+        timeoutRef.current = setTimeout(() => {
+            callback(...args);
+        }, delay);
+    }, [callback, delay]);
+
+    return debouncedCallback;
+}
