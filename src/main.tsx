330b240b5747f3bee35645493e956c28d4163e37
PriyamSrivastava1507 <priyam.srivastava1575@gmail.com>
2026-05-07 23:55:29 +0530

    feat: add routing via react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,40 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+
+import { createBrowserRouter} from "react-router";
+import { RouterProvider } from 'react-router';
+
+import FormBuilderPage from './pages/FormBuilderPage';
+import FormOutputPage from './pages/FormOutputPage';
+import RootLayout from './layouts/RootLayout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <FormBuilderPage />
+      },
+      {
+        path: "format-selection",
+        element: <FormBuilderPage />
+      },
+      {
+        path: "output",
+        element: <FormOutputPage />
+      }
+    ]
+  }
+])
 
 // Primary application entry point that mounts the React app to the root DOM node
 const rootElement = document.getElementById('root') as HTMLElement;
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
