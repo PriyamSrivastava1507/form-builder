83073274e5aa85da9507bd0707d50640ea65195d
PriyamSrivastava1507 <priyam.srivastava1575@gmail.com>
2026-05-10 10:22:34 +0530

    refactor: move Field Palette inside seperate Form Builder directory

---
+++ src/components/FormBuilder/FieldPalette/FieldPaletteGroup.tsx
@@ -0,0 +1,29 @@
+import type { PaletteGroupConfig } from "../../../types/palette"
+import type { FieldSchema } from "../../../types/field"
+import FieldPaletteItem from "./FieldPaletteItem";
+
+type FieldPaletteGroupProps = {
+    group: PaletteGroupConfig;
+    addField: (field: FieldSchema) => void;
+}
+
+/**
+ * @component
+ * @param {FieldPaletteGroupProps} props - Component props
+ * Renders a specific category group of form fields in the palette
+ */
+const FieldPaletteGroup = ({ group, addField }: FieldPaletteGroupProps) => {
+  const { label, items } = group;
+  return (
+    <section>
+        <h3 className="text-sm font-semibold text-foreground-muted ml-2 mt-2 mb-2">{label.toLocaleUpperCase()}</h3>
+        <div className="ml-4">
+            {items.map((item) => (
+                <FieldPaletteItem key={item.label} item={item} addField={addField} />
+            ))}
+        </div>
+    </section>
+  )
+}
+
+export default FieldPaletteGroup
